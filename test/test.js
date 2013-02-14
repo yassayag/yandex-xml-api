500d67ccaba1f6aee6a2a5908d24ae271019361a
yassayag <yoann.assayag@gmail.com>
2013-02-14 19:47:13 +0000

    basic projet to get the 10 first URL from yandex

---
+++ test/test.js
@@ -0,0 +1,40 @@
+var assert = require("assert")
+  , fs = require("fs")
+  , YandexSearch = require("../lib/yandex-xml-api")
+
+describe('Result processing', function(){
+  describe('Normal response', function(){
+    it('should return no error in case of normal response', function(){
+	  var XMLResponse = fs.readFileSync("./test/responseMokup.xml");
+	  var yandexSearch = new YandexSearch();
+	  yandexSearch.processResult(XMLResponse, function(err, result){
+		if(err) console.log(err)
+		assert.ok(result, "no result returned");
+		console.log(result)
+	  })
+	  
+    })/*
+	it('should return no error in case of normal response', function(){
+	  var XMLResponse = fs.readFileSync("./test/responseMokup.xml");
+	  var yandexSearch = new YandexSearch();
+	  yandexSearch.getWebResult(XMLResponse, function(err, result){
+		if(err) console.log(err)
+		assert.ok(result, "no result returned");
+		console.dir(result)
+	  })
+	  
+    })*/
+  })
+    describe('No result response', function(){
+    it('should an error in case of no result', function(){
+	  var XMLResponse = fs.readFileSync("./test/noResult.xml");
+	  var yandexSearch = new YandexSearch();
+	  yandexSearch.processResult(XMLResponse, function(err, result){
+		if(err) console.log(err)
+		assert.ok(err, "no error is retuen when no result");
+		console.log (err)
+	  })
+	  
+    })
+  })
+})
